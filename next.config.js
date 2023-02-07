50fc3e575c39bdf11287580b768490d68ea540da
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-07 20:17:01 +0100

    Attempt env var fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-const webpack = require("webpack");
+// const webpack = require("webpack");
 
-const { parsed: myEnv } = require("dotenv").config();
+// const { parsed: myEnv } = require("dotenv").config();
 
 const nextConfig = {
   reactStrictMode: true,
@@ -12,18 +12,18 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
-  env: {
-    API_KEY: process.env.BASE_URL,
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    CONTACTS_MAIL_ADDRESS_FROM: process.env.CONTACTS_MAIL_ADDRESS_FROM,
-    CONTACTS_MAIL_ADDRESS_TO: process.env.CONTACTS_MAIL_ADDRESS_TO,
-    JOIN_US_MAIL_ADDRESS_FROM: process.env.JOIN_US_MAIL_ADDRESS_FROM,
-    JOIN_US_MAIL_ADDRESS_TO: process.env.JOIN_US_MAIL_ADDRESS_TO,
-  },
+  // webpack(config) {
+  //   config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+  //   return config;
+  // },
+  // env: {
+  //   API_KEY: process.env.BASE_URL,
+  //   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  //   CONTACTS_MAIL_ADDRESS_FROM: process.env.CONTACTS_MAIL_ADDRESS_FROM,
+  //   CONTACTS_MAIL_ADDRESS_TO: process.env.CONTACTS_MAIL_ADDRESS_TO,
+  //   JOIN_US_MAIL_ADDRESS_FROM: process.env.JOIN_US_MAIL_ADDRESS_FROM,
+  //   JOIN_US_MAIL_ADDRESS_TO: process.env.JOIN_US_MAIL_ADDRESS_TO,
+  // },
   optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl =
-  process.env.NODE_ENV === "production"
+  process?.env?.NODE_ENV === "production"
     ? // ? 'https://trynext-react.envytheme.com'
-      ""
+      "/"
     : "http://localhost:3000";
 
 export default baseUrl;
