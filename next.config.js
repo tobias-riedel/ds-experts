a3dda2d0ebbf33c33ef70a4f8d076c9cac47fbe9
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-09 19:44:24 +0100

    Removed dotenv npm package

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-// const webpack = require("webpack");
-
-// const { parsed: myEnv } = require("dotenv").config();
 
 const nextConfig = {
   reactStrictMode: true,
@@ -11,23 +8,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  // webpack(config) {
-  //   config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-  //   return config;
-  // },
-  // env: {
-  //   API_KEY: process.env.BASE_URL,
-  //   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  //   CONTACTS_MAIL_ADDRESS_FROM: process.env.CONTACTS_MAIL_ADDRESS_FROM,
-  //   CONTACTS_MAIL_ADDRESS_TO: process.env.CONTACTS_MAIL_ADDRESS_TO,
-  //   JOIN_US_MAIL_ADDRESS_FROM: process.env.JOIN_US_MAIL_ADDRESS_FROM,
-  //   JOIN_US_MAIL_ADDRESS_TO: process.env.JOIN_US_MAIL_ADDRESS_TO,
-  // },
   optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    // ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
 };
 
