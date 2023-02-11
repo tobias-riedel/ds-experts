3f8b4674298a46d8fbb239152fd6e6972beb18e7
Tobias Riedel <45273391+tobias-riedel@users.noreply.github.com>
2023-02-11 02:00:30 +0100

    Added honeypot to contact form (#13)

--- components/OurWorks.js
+++ components/OurWorks.js
@@ -15,7 +15,7 @@ const references = [
     tasks: [
       "Projektleitung IT-Rollout",
       "Projektleitung Erstellung Engineering Cloud",
-      // "AufbauGültigkeitsmanagement in Teamcenter",
+      // "Aufbau Gültigkeitsmanagement in Teamcenter",
     ],
   },
   {
@@ -51,10 +51,6 @@ const OurWorks = () => {
         <div className="container-fluid">
           <div className="section-title">
             <h2>Referenzen</h2>
-            {/* <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p> */}
           </div>
 
           <Swiper
@@ -86,25 +82,6 @@ const OurWorks = () => {
             modules={[Navigation, Pagination, Mousewheel, Keyboard]}
             className="work-slides"
           >
-            {/* <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work1.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Panalpina AG (Basel)</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Erstellung Softwarelösung in Exchange</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide> */}
             {references.map((ref, idx) => (
               <SwiperSlide key={idx}>
                 <div className="work-card">
@@ -130,165 +107,6 @@ const OurWorks = () => {
                 </div>
               </SwiperSlide>
             ))}
-
-            {/* <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work1.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Panalpina AG (Basel)</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Erstellung Softwarelösung in Exchange</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work2.jpg" alt="image" />
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Web Design</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Building design process within teams</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work3.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>eCommerce</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>How intercom brings play eCommerce</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work4.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Reactjs</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>How to start a project with Reactjs</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work5.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Angular js</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Examples of different types of sprints</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work6.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>UI/UX Design</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Redesigning the New York times app</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work7.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Graphic Design</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Design the Web, Mobile, and eCommerce</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="work-card">
-                <img src="/images/works/work8.jpg" alt="image" />
-
-                <div className="content text-center">
-                  <span>
-                    <Link href="/portfolio">
-                      <a>Bootstrap</a>
-                    </Link>
-                  </span>
-
-                  <h3>
-                    <Link href="/portfolio-details">
-                      <a>Redesigning the New York times app</a>
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide> */}
           </Swiper>
         </div>
       </section>
