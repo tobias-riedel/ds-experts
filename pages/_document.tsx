77e83d2fdfb0285b7af41c67bfe8d43b983b281c
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-16 21:27:01 +0100

    Implemented local fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,20 +9,7 @@ class MyDocument extends Document {
     return (
       <Html lang="de">
         {/* TODO: Add keywords for SEO */}
-        {/* TODO: Use font optimization */}
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Saira:100,200,300,400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
           <link rel="icon" type="image/png" href="/favicon.png"></link>
         </Head>
         <body>
