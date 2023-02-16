eca0e29b0ea171466ff73a4efc54db1d3cf2cac6
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-16 14:31:31 +0100

    Attempted mobile navbar flickering fix

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import Link from "../../utils/ActiveLink";
+import { isMobile } from "react-device-detect";
 
 interface NavLinks {
   to: string;
@@ -83,7 +84,7 @@ const Navbar = () => {
   const logoAltText = "ds-experts IT-Consulting GmbH";
 
   return (
-    <header className={`scroll-${scrollDirection}`}>
+    <header className={`${isMobile && "is-mobile"} scroll-${scrollDirection}`}>
       <div id="navbar" className="navbar-area navbar-style-2">
         <nav role="navigation" className="navbar navbar-expand-md navbar-light">
           <div className="container-fluid">
