cf608ceb507644989895d0e070004ba8530b93ff
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-19 19:08:24 +0100

    Fixed WorkProcess section padding bottom

--- components/WorkProcess.tsx
+++ components/WorkProcess.tsx
@@ -2,71 +2,69 @@ import SectionDivider from "./Common/SectionDivider";
 
 const WorkProcess = () => {
   return (
-    <>
-      <section className="work-process-area pt-100">
-        <div className="container">
-          <div className="section-title">
-            <h2>Unser Erfolg kommt von...</h2>
-          </div>
-
-          <div className="work-process">
-            <img src="/images/man-with-mobile.png" alt="logo" />
-
-            <div className="work-process-list">
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-display1"></i>
-                </div>
-                <h3>Know-How</h3>
-              </div>
-
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-display2"></i>
-                </div>
-                <h3>Resilienz</h3>
-              </div>
-
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-study"></i>
-                </div>
-                <h3>Ausbildung</h3>
-              </div>
-
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-id"></i>
-                </div>
-                <h3>Professionalität</h3>
-              </div>
-
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-diamond"></i>
-                </div>
-                <h3>Entschlossenheit</h3>
-              </div>
-
-              <div className="single-work-process">
-                <div className="icon">
-                  <i className="pe-7s-gift"></i>
-                </div>
-                <h3>Qualität</h3>
-              </div>
-            </div>
-
-            <img
-              src="/images/circle.png"
-              className="rotateme circle-image"
-              alt="image"
-            />
-          </div>
+    <section className="pt-100">
+      <div className="container work-process-area">
+        <div className="section-title">
+          <h2>Unser Erfolg kommt von...</h2>
         </div>
 
-        <SectionDivider />
-      </section>
-    </>
+        <div className="work-process">
+          <img src="/images/man-with-mobile.png" alt="logo" />
+
+          <div className="work-process-list">
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-display1"></i>
+              </div>
+              <h3>Know-How</h3>
+            </div>
+
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-display2"></i>
+              </div>
+              <h3>Resilienz</h3>
+            </div>
+
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-study"></i>
+              </div>
+              <h3>Ausbildung</h3>
+            </div>
+
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-id"></i>
+              </div>
+              <h3>Professionalität</h3>
+            </div>
+
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-diamond"></i>
+              </div>
+              <h3>Entschlossenheit</h3>
+            </div>
+
+            <div className="single-work-process">
+              <div className="icon">
+                <i className="pe-7s-gift"></i>
+              </div>
+              <h3>Qualität</h3>
+            </div>
+          </div>
+
+          <img
+            src="/images/circle.png"
+            className="rotateme circle-image"
+            alt="image"
+          />
+        </div>
+      </div>
+
+      <SectionDivider />
+    </section>
   );
 };
 
