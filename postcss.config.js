1102aba984834b222df390f0619b56dfa08f4be9
Tobias Riedel <45273391+tobias-riedel@users.noreply.github.com>
2023-02-22 01:42:11 +0100

    Purged unuused CSS (#44)

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+          "./app/**/*.{js,jsx,ts,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+    "postcss-preset-env",
+  ],
+};
