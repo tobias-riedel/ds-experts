a9a26984ad76f24fc8401e9ebde61f6a9d32c411
Tobias Riedel <tobias.riedel@healex.systems>
2023-02-22 12:28:42 +0100

    Attempted CSS purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,31 @@
+// module.exports = {
+//   plugins: [
+//     [
+//       "@fullhuman/postcss-purgecss",
+//       {
+//         content: [
+//           "./pages/**/*.{js,jsx,ts,tsx}",
+//           "./components/**/*.{js,jsx,ts,tsx}",
+//           "./app/**/*.{js,jsx,ts,tsx}",
+//         ],
+//         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+//       },
+//     ],
+//     "postcss-preset-env",
+//   ],
+// };
+
 module.exports = {
-  plugins: [
-    [
-      "@fullhuman/postcss-purgecss",
-      {
-        content: [
-          "./pages/**/*.{js,jsx,ts,tsx}",
-          "./components/**/*.{js,jsx,ts,tsx}",
-          "./app/**/*.{js,jsx,ts,tsx}",
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  plugins: {
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
       },
-    ],
-    "postcss-preset-env",
-  ],
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+  },
 };
