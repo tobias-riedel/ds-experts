a111ca427b41632893dca01a811ab31ed22b5016
Tobias Riedel <tobias.riedel@healex.systems>
2023-03-02 14:07:42 +0100

    Removed more obsolete legal texts

--- components/Gdpr.tsx
+++ components/Gdpr.tsx
@@ -41,33 +41,7 @@ const Gdpr = () => {
           Datenübertragung im Internet, wie zum Beispiel über E-Mail, kann immer Sicherheitslücken aufweisen. Der
           komplette Schutz der Daten ist im Internet nicht möglich.
         </p>
-        <h3>Datenschutzerklärung Google Analytics</h3>
-        <p>
-          Unsere Website nutzt den Analysedienst Google Analytics, betrieben von Google Inc. 1600 Amphitheatre Parkway
-          Mountain View, CA 94043, USA. Google Analytics nutzt “Cookies”, das sind kleine Textdateien, die in Ihrem
-          Browser gespeichert werden und die es ermöglichen, die Nutzung unserer Website durch die Besucher zu
-          analysieren. Von den Cookies werden Daten über Ihre Nutzung unserer Webseite gesammelt, die normalerweise an
-          einen Google-Server in den USA übertragen und gespeichert werden.
-        </p>
-        <p>
-          Wenn die IP-Anonymisierung auf unserer Webseite aktiviert wurde, wird Ihre IP-Adresse von Google innerhalb der
-          Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen
-          Wirtschaftsraum vorab gekürzt. In seltenen Ausnahmefällen kann die komplette IP-Adresse an einen Google Server
-          in den USA übertragen werden, dann wird diese dort gekürzt. Google nutzt diese Daten in unserem Auftrag, um
-          die Nutzung unserer Website auszuwerten, um Berichte über die Webseitenaktivitäten zu erstellen sowie um
-          weitere Dienstleistungen anzubieten, die mit der Webseitennutzung und Internetnutzung zusammenhängen. Die von
-          Google Analytics erfassten IP-Adressen werden nicht mit anderen Daten von Google korreliert.
-        </p>
-        <p>
-          Die Speicherung von Cookies kann durch eine spezielle Einstellung in Ihrem Browser verweigert werden. In
-          diesem Fall ist jedoch die Funktionalität unserer Webseite im vollen Umfang nicht gewährleistet. Zusätzlich
-          steht ihnen ein Browser-Plugin zu Verfügung, mit dem Sie die Sammlung der auf Ihre Nutzung der Website
-          bezogenen erzeugten Daten und IP-Adressen durch Google verhindern können. Mehr Informationen dazu finden Sie
-          hier:{' '}
-          <Link href="http://tools.google.com/dlpage/gaoptout?hl=de" target="_blank" rel="noopener">
-            http://tools.google.com/dlpage/gaoptout?hl=de
-          </Link>
-        </p>
+
         <h3>Auskunft, Löschung, Sperrung</h3>
         <p>
           Zu jedem Zeitpunkt können Sie sich über die personenbezogenen Daten, deren Herkunft und Empfänger und den
@@ -93,23 +67,7 @@ const Gdpr = () => {
           Datenquellen. Wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden behalten wir uns
           das Recht vor, diese Daten nachträglich zu überprüfen.
         </p>
-        <h3>Cookies</h3>
-        <p>
-          Viele Internetseiten verwenden Cookies. Cookies sind unschädlich für Ihren Rechner und virenfrei. Sie dienen
-          dazu, Internet-Angebote für die Besucher einer Webseite freundlicher, effektiver und sicherer zu machen.
-          Cookies sind kleine Textdateien, die auf Ihrem Computer abgelegt werden und die Ihr Browser verwendet.
-        </p>
-        <p>
-          Wir verwenden in der Regel so genannte „Session-Cookies“. Diese werden nach Verlassen unserer Seite
-          automatisch gelöscht. Andere Cookies bleiben auf Ihrem Computer gespeichert, bis Sie diese löschen. Diese
-          Cookies helfen dabei, Ihren Rechner beim nächsten Besuch wiederzuerkennen.
-        </p>
-        <p>
-          Über die Browsereinstellungen können sie festlegen, dass Sie über neue Cookies informiert werden und Cookies
-          jeweils annehmen müssen. Ebenso können Sie die Annahme von Cookies für bestimmte Fälle oder generell
-          ausschließen oder das automatische Löschen der Cookies beim Schließen des Browser aktivieren. Werden Cookies
-          desaktiviert, kann die Funktionalität unserer Website eingeschränkt sein.
-        </p>
+
         <h3>Formulare</h3>
         <p>
           Daten, die uns über unsere Formulare erreichen, werden inklusive des Inhalts der Anfrage für
