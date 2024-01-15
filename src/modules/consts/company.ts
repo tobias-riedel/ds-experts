29017d084ffe37be782c4d13c492f09e4f9c752a
Tobias Riedel <tobias.riedel@healex.systems>
2024-01-15 13:31:44 +0100

    Updated company adress

--- src/modules/consts/company.ts
+++ src/modules/consts/company.ts
@@ -1,12 +1,12 @@
 export const COMPANY_FULL_NAME = 'ds-experts IT-Consulting GmbH';
 
 export const COMPANY_ADDRESS = {
-  street: 'Rudolf-Breitscheid-Straße',
-  streetNo: '68',
+  street: 'Lindenstraße',
+  streetNo: '12',
   zipCode: '16559',
   city: 'Liebenwalde',
   telephone: '+49 33054 904 404',
   email: 'info@ds-experts.de',
 };
 
-export const COMPANY_COORDINATES: [number, number] = [52.87365, 13.3835];
+export const COMPANY_COORDINATES: [number, number] = [52.8762031, 13.4071721];
