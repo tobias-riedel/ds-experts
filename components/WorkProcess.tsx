23fcb9970a88790d5e560f87f4d9f4b2cfbe925e
Tobias Riedel <tobias.riedel@healex.systems>
2024-02-09 17:07:20 +0100

    Added texts to work process

--- components/WorkProcess.tsx
+++ components/WorkProcess.tsx
@@ -3,6 +3,39 @@ import mobileImg from '../public/images/man-with-mobile.png';
 import circleImg from '../public/images/circle.png';
 import SectionDivider from './Common/SectionDivider';
 
+const data = [
+  {
+    iconName: 'pe-7s-display1',
+    title: 'Know-How',
+    text: 'Kenntnisse, Erfahrungen, Wettbewerbsvorteil und umfangreiches Fachwissen',
+  },
+  {
+    iconName: 'pe-7s-display2',
+    title: 'Resilienz',
+    text: 'Widerstandsfähig, Lösungsorientiert, Akzeptanz und Optimismus',
+  },
+  {
+    iconName: 'pe-7s-study',
+    title: 'Fortbildung',
+    text: 'Lehrgänge, Meisterkurse und gefestigte Qualifikation',
+  },
+  {
+    iconName: 'pe-7s-id',
+    title: 'Professionalität',
+    text: 'hochwertige Arbeitsleistung, gekonntes Verhalten und souveränes Handeln',
+  },
+  {
+    iconName: 'pe-7s-diamond',
+    title: 'Entschlossenheit',
+    text: 'fester Wille oder Willenskraft, Antrieb, definierte Ziele und Ergebnisorientiert',
+  },
+  {
+    iconName: 'pe-7s-gift',
+    title: 'Qualität',
+    text: 'unser Merkmal, Güte unserer Eigenschaft und zufriedenstellende Ausführungen',
+  },
+];
+
 const WorkProcess = () => {
   return (
     <section className="pt-100">
@@ -22,47 +55,15 @@ const WorkProcess = () => {
           />
 
           <div className="work-process-list">
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-display1"></i>
+            {data.map((value, idx) => (
+              <div className="single-work-process" key={idx}>
+                <div className="icon">
+                  <i className={value.iconName}></i>
+                </div>
+                <h3>{value.title}</h3>
+                <span>{value.text}</span>
               </div>
-              <h3>Know-How</h3>
-            </div>
-
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-display2"></i>
-              </div>
-              <h3>Resilienz</h3>
-            </div>
-
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-study"></i>
-              </div>
-              <h3>Weiterbildung</h3>
-            </div>
-
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-id"></i>
-              </div>
-              <h3>Professionalität</h3>
-            </div>
-
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-diamond"></i>
-              </div>
-              <h3>Entschlossenheit</h3>
-            </div>
-
-            <div className="single-work-process">
-              <div className="icon">
-                <i className="pe-7s-gift"></i>
-              </div>
-              <h3>Qualität</h3>
-            </div>
+            ))}
           </div>
 
           <Image
