cf65500b7ce051a55a39c4a600f9b2bd827b35fc
Tobias Riedel <riedel.tbs@gmail.com>
2024-11-22 10:08:39 +0100

    Added former employee styling

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -68,7 +68,7 @@ const Team = ({ data: experts }: { data: Expert[] }): JSX.Element => {
                 <h3 className="text-center">Ehemalige Experts</h3>
                 <div className="row justify-content-between">
                   {formerExperts?.map((expert, idx) => (
-                    <div className="col-lg-2 col-md-4 col-6" key={idx}>
+                    <div className="col-lg-2 col-md-4 col-6 former-employees" key={idx}>
                       <ExpertCard data={expert} />
                     </div>
                   ))}
